828504d5427708578a9f5f557f1cff6051752873
uraway <masato.u0107@gmail.com>
2017-03-02 21:51:22 +0900

    refactor

--- src/OauthGithub.js
+++ src/OauthGithub.js
@@ -8,22 +8,25 @@ export default class AuthWindow {
   constructor({ id, secret, scopes = [] }) {
     assert(id, 'Client ID is needed!');
     assert(secret, 'Client Secret is needed!');
-    this.scopeQuery = scopes.length > 0 ? '&scope=' + scopes.join('%2C') : '';
+    this.scopes = scopes;
     this.clientId = id;
     this.clientSecret = secret;
     this.window = null;
+    this.callback = null;
   }
 
   startRequest(callback) {
+    this.callback = callback;
     app.on('ready', () => {
       this.window = new BrowserWindow({ width: 800, height: 600, 'node-integration': false });
-      var authURL = 'https://github.com/login/oauth/authorize?client_id=' + this.clientId + this.scopeQuery;
+      var authURL = 'https://github.com/login/oauth/authorize?client_id=' + this.clientId + '&scope=' + this.scopes;
       this.window.loadURL(authURL);
+      this.window.show();
       this.window.webContents.on('will-navigate', (event, url) => {
-        this.handleCallback(url, callback);
+        this.handleCallback(url);
       });
       this.window.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
-        this.handleCallback(newUrl, callback);
+        this.handleCallback(newUrl);
       });
       this.window.on('close', () => {
         this.window = null;
@@ -31,30 +34,29 @@ export default class AuthWindow {
     });
   }
 
-  handleCallback(url, callback) {
-    let raw_code = /code=([^&]*)/.exec(url) || null;
-    let code = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
-    let error = /\?error=(.+)$/.exec(url);
-
+  handleCallback (url) {
+    var raw_code = /code=([^&]*)/.exec(url) || null;
+    var code = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
+    var error = /\?error=(.+)$/.exec(url);
     if (code) {
-      this.requestGithubToken(this.clientId, this.clientSecret, this.scopeQuery, code, callback);
+      this.requestGithubToken(code);
     } else if (error) {
-      console.log(error);
+      alert('Oops! Something went wrong and we couldn\'t' +
+        'log you in using Github. Please try again.');
     }
   }
 
-  requestGithubToken(id, secret, scopes, code, callback) {
+  requestGithubToken(code) {
     request.post('https://github.com/login/oauth/access_token', {
-      client_id: id,
-      client_secret: secret,
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
       code: code,
     }).end((err, response) => {
       if (err) {
-        callback(err);
+        this.callback(err);
       }
-
       var access_token = response.body.access_token;
-      callback(response.body.access_token);
+      this.callback(response.body.access_token);
     });
   }
 
